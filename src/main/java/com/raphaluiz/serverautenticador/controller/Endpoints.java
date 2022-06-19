cd28081c4cd1c6f76ab27070e4fcdd58b32ceb1d
Raphael Silva <raphael.silva@elastacloud.com>
2022-06-19 17:33:48 -0300

    Update endpoints

--- src/main/java/com/raphaluiz/serverautenticador/controller/Endpoints.java
+++ src/main/java/com/raphaluiz/serverautenticador/controller/Endpoints.java
@@ -1,7 +1,9 @@
 package com.raphaluiz.serverautenticador.controller;
 
 import com.google.zxing.WriterException;
+import com.raphaluiz.serverautenticador.model.Auth2FA;
 import com.raphaluiz.serverautenticador.model.AuthData;
+import com.raphaluiz.serverautenticador.model.Response2FA;
 import com.raphaluiz.serverautenticador.model.entity.Person;
 import com.raphaluiz.serverautenticador.repository.PersonRepository;
 import com.raphaluiz.serverautenticador.service.ScryptService;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.Optional;
 
+import static org.springframework.http.ResponseEntity.ok;
+
 @RestController
 @AllArgsConstructor
 public class Endpoints {
@@ -24,40 +28,62 @@ public class Endpoints {
 
 
     @PostMapping("/create-user")
-    @ResponseBody
-    ResponseEntity<String> createUser(@RequestBody AuthData authData) {
+    ResponseEntity createUser(@RequestBody AuthData authData) {
 
         Optional<Person> optionalPerson = personRepository.getByName(authData.getUserName());
 
         if (optionalPerson.isPresent())
-            return ResponseEntity.ok("Usuário existente");
+            return new ResponseEntity<>("Usuário existente", HttpStatus.BAD_REQUEST);
 
         String encodedAuthToken = scryptService.encode(authData.getAuthToken());
+        String secretKey = twoFactorAuthService.generateSecretKey();
 
         Person person = new Person();
 
         person.setName(authData.getUserName());
         person.setAuthToken(encodedAuthToken);
+        person.setOTPSecret(secretKey);
 
         personRepository.save(person);
 
-        String secretKey = twoFactorAuthService.generateSecretKey();
-        String email = "email@gmail.com";
-        String companyName = "Empresa";
+        String email = "raphaluiz@gmail.com";
+        String companyName = "Raphael&Luiz";
         String barCodeUrl = twoFactorAuthService.getGoogleAuthenticatorBarCode(secretKey, email, companyName);
 
-        return ResponseEntity.ok(barCodeUrl);
+        return ResponseEntity.ok(new Response2FA(barCodeUrl));
     }
-    @PostMapping("/two-factor-auth")
-    ResponseEntity twoFactorAuthenticateUser(@RequestBody String securityCode) {
-        String secretKey = System.getenv().get("SECRET_KEY_TWO_FACTOR");
 
-        String TOTPcode = twoFactorAuthService.getTOTPCode(secretKey);
+    @PostMapping("/auth")
+    ResponseEntity authenticateUser(@RequestBody AuthData authData) {
 
-        if (securityCode.equals(TOTPcode)) {
-            return ResponseEntity.ok(null);
+        String encodedAuthToken = scryptService.encode(authData.getAuthToken());
+        Optional<Person> optionalPerson = personRepository.getByName(authData.getUserName());
+
+        if (optionalPerson.isPresent()) {
+            Person person = optionalPerson.get();
+            if (person.getAuthToken().equals(encodedAuthToken)) {
+                return ResponseEntity.ok("Please, verify your OTP code!");
+            }
         }
+        return new ResponseEntity<>("Ops, wrong password!!!", HttpStatus.UNAUTHORIZED);
+    }
 
+
+    @PostMapping("/two-factor-auth")
+    ResponseEntity twoFactorAuthenticateUser(@RequestBody Auth2FA auth2fa) {
+
+        Optional<Person> optionalPerson = personRepository.getByName(auth2fa.getUserName());
+
+        if (optionalPerson.isPresent())
+            return new ResponseEntity<>("Usuário existente", HttpStatus.BAD_REQUEST);
+
+        String secretKey = optionalPerson.get().getOTPSecret();
+
+        String OTPcode = twoFactorAuthService.getTOTPCode(secretKey);
+
+        if (auth2fa.getOTPCode().equals(OTPcode)) {
+            return ok(null);
+        }
         return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
     }
 }
